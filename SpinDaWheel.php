04357b0011f502e0bce7c70cb967e4211b474dc9
PENGILLY <jp0971865@dt050585.otc.edu>
2024-08-28 11:12:03 -0500

    https://stackoverflow.com/questions/676677/how-can-i-add-elements-to-an-empty-array-in-php#:~:text=You%20can%20use%20array_push.,array%2C%20like%20in%20a%20stack. this website is what we need

--- SpinDaWheel.php
+++ SpinDaWheel.php
@@ -1,27 +1,22 @@
 <?php
-$outCome = ['a' =>"A", 'b'=>"B", 'c'=>"C"];
-$roll = "";
+$outCome = ['A' =>"A", 'B'=>"B", 'C'=>"C"];
+$totalarray = [];
 $temp = "";
+$space =" ";
 $i = 0;
+$t = 0;
+$return = 0;
 $total = 0;
-while ($total < 500 && $i<21){
-    for ($i=0; $i < 21;$i++ )
+while ($total < 500 && $t<20){
+    for ($i=0; $i < 3; $i++ )
     {
-
-        $temp = array_rand($outCome);
-        $roll = $roll . $outCome[$temp];
-        $temp = array_rand($outCome);
-        $roll = $roll . $outCome[$temp];
-        $temp = array_rand($outCome);
-        $roll = $roll . $outCome[$temp];
-        echo($roll . " : ");
-        $return_value = match ($roll) {
-            "AAA","BBB", "CCC"=> 100,
-            "AAB", "ABA", "BAA", "ABB", "BBA", "BAB", "BCC", "CBC", "CCB", "ACC", "CAC", "CCA" => 50
-        }
-        $total =+ $return_value;
-
-        $roll = "";
-        
+        $temp .= array_rand($outCome);
     }
+    $return_value = match ($temp) {
+        "AAA","BBB", "CCC"=> 100,
+        "AAB", "ABA", "BAA", "ABB", "BBA", "BAB", "BCC", "CBC", "CCB", "ACC", "CAC", "CCA", "CAA", "BBC", "CBB", "AAC", "BCB", "ACA"=> 50,
+        "ABC", "CAB", "BAC", "ACB", "CBA", "BCA" => 0,
+    };
+    $temp = "";
 }
+echo($total);
